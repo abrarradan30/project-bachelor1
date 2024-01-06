72e6565f1bd62bb67b19d6240fa0ac26f2cb00fd
abrarradan30 <abradan84@gmail.com>
2024-01-06 19:53:43 +0700

    layout

--- resources/views/admin/layout/appadmin.blade.php
+++ resources/views/admin/layout/appadmin.blade.php
@@ -1,6 +1,19 @@
-@include('admin.layout.top')
-@include('admin.layout.menu')
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+    @include('admin.layout.menu')
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+            @include('admin.layout.top')
+            @yield('content')
+            </div>
+            <!-- End of Main Content -->
+            @include('admin.layout.bottom')
+        </div>
+        <!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Page Wrapper -->
+</body>
 
-@yield('content')
-
-@include('admin.layout.bottom')
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('admin/dashboard');
 });
+
+Route::get('/detail_materi', function () {
+    return view('admin/detail_materi/index');
+});

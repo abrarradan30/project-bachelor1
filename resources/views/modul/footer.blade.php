19194a5eccd251be4b07cbb923242ced9573ff61
abrarradan30 <abradan84@gmail.com>
2024-04-16 22:12:03 +0700

    add files for module requirements

---
+++ resources/views/modul/footer.blade.php
@@ -0,0 +1,12 @@
+<!-- Vendor JS Files -->
+<script src="{{ asset('modul/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('modul/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ asset('modul/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+<script src="{{ asset('modul/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('modul/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('modul/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ asset('modul/assets/vendor/aos/aos.js') }}"></script>
+<script src="{{ asset('modul/assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('modul/assets/js/main.js') }}"></script>
---
+++ resources/views/modul/index.blade.php
@@ -0,0 +1,7 @@
+<body class="services-details-page" data-bs-spy="scroll" data-bs-target="#navmenu">
+@include('modul.header')
+
+@yield('content')
+
+@include('modul.footer')
+</body>
